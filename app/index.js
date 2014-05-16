9c931d0b6a565a92f7895a0e6ef14c9ee18cc78e
Matthieu Verrecchia <matthieu.verrecchia@gmail.com>
2014-05-16 12:38:28 +0200

    Added dropzone

--- app/index.js
+++ app/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+
+app.use(express.static('public'));
+
 app.set('views', __dirname + '/views');
 app.get('/', function(req, res) {
 	res.render('index.ejs', {});
 });
+app.post('/upload', function(req, res) {
+    console.log('Uploaded');
+    //res.render('uploaded.ejs', {});
+});
+
 
 app.listen(1337);
