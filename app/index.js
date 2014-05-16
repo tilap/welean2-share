40f8233eb14367908da9d3be0f8c8c55eeefc56c
Yvan Wibaux <yvan@evaneos.com>
2014-05-16 11:55:26 +0200

    express init

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337);
-console.log('hello');
+var express = require('express');
+var app = express();
+app.set('views', __dirname + '/views');
+app.get('/', function(req, res) {
+	res.render('index.ejs', {});
+});
+
+app.listen(1337);
