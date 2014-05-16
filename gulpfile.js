3c0690f38c69add65509cbbcaa9772e2359d706a
Yvan Wibaux <yvan@evaneos.com>
2014-05-16 11:34:56 +0200

    hello world

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
+var child_process = require('child_process');
+var daemon = require('springbokjs-daemon').node([ 'app/index.js' ]);
+
+process.on('exit', function(code) {
+    daemon.stop();
+});
+
 
 gulp.task('watch', function() {
-	gulp.watch('src/**/*.js').on('change', function(file) {
-		node('index.js');
-	})
+    daemon.start();
+    gulp.watch('app/**/*.js').on('change', function() {
+        daemon.restart();
+    });
 });
