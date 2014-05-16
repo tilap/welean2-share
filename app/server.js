fec3d587fb694877615bfe2d96bd121ed92a030d
Matthieu Verrecchia <matthieu.verrecchia@gmail.com>
2014-05-17 00:30:41 +0200

    Config file

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 require('newrelic');
+var config = require('../config/app');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var mongo = require('mongodb');
@@ -41,4 +42,4 @@ app.post('/:uuid/upload/', require('./controllers/uploadController.js'));
 /***********
 * GOOOO
 ************/ 
-app.listen(1337);
+app.listen(config.port);
