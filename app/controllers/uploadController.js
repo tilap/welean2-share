2403ecd8547b8f34cb7f26efc254dd91f972c72c
Yvan Wibaux <yvan@evaneos.com>
2014-05-16 16:13:37 +0200

    clean index.js

--- app/controllers/uploadController.js
+++ app/controllers/uploadController.js
@@ -1,3 +1,11 @@
+var upload = require('../utils/upload');
+
 module.exports = function(req, res) {
-	
-}
+
+    upload('./tmp', req, function(){
+        console.log('Retour ok 200');
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end();
+    });
+
+};
--- app/index.js
+++ app/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
-
-var app = express();
-
 var cookieParser = require('cookie-parser');
 var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
-
 require('springbokjs-shim/es6');
-// Connect to the db
+
+/***********
+* DB
+************/
 var db = null;
 MongoClient.connect("mongodb://localhost:27017/share", function(err, dbMongo) {
   if(!err) {
@@ -16,31 +15,29 @@ MongoClient.connect("mongodb://localhost:27017/share", function(err, dbMongo) {
   }
 });
 
+/***********
+* APP
+************/
+var app = express();
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
+app.use(cookieParser());
+
 // Make our db accessible to our router
 app.use(function(req,res,next){
     req.db = db;
     next();
 });
 
-
-app.use(express.static('public'));
-
-app.set('views', __dirname + '/views');
-
-app.use(cookieParser());
-
+/***********
+* ROUTES
+************/
 app.get('/:uuid', require('./controllers/albumController.js'));
 app.get('/', require('./controllers/indexController.js'));
-
-var upload = require('./utils/upload');
-app.post('/upload', function(req, res) {
-    upload('./tmp', req, function(){
-        console.log('Retour ok 200');
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end();
-    });
-    //res.render('uploaded.ejs', {});
-});
+app.post('/upload', require('./controllers/uploadController.js'));
+    
  
-
+/***********
+* GOOOO
+************/ 
 app.listen(1337);
