8c425178bc0d2629423aa60de7dff12f1c272087
Yvan Wibaux <yvan@evaneos.com>
2014-05-17 02:34:32 +0200

    change directory structure add less

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp');
 var child_process = require('child_process');
-var daemon = require('springbokjs-daemon').node([ 'app/server.js' ]);
+var daemon = require('springbokjs-daemon').node([ 'src/server/server.js' ]);
+var less = require("gulp-less");
 
 process.on('exit', function(code) {
     daemon.stop();
 });
 
-
-gulp.task('watch', function() {
+gulp.task('watch', ['less'], function() {
     daemon.start();
-    gulp.watch('app/**/*.js').on('change', function() {
+    gulp.watch('src/server/**/*.js').on('change', function() {
         daemon.restart();
     });
+    gulp.watch('src/browser/**/*.less', ['less']);
 });
+gulp.task('less', function() {
+	gulp.src('src/browser/less/style.less').pipe(less()).pipe(gulp.dest('public/dist'));
+})
