f3102cd4cbd896821c10ea33369d2c4c0b6f1119
Christophe Hurpeau <christophe@hurpeau.com>
2014-05-19 18:13:54 +0200

    Update albumController.js

--- src/server/controllers/albumController.js
+++ src/server/controllers/albumController.js
@@ -3,16 +3,16 @@ var AlbumFactory = require(config.serverDir + '/model/album/AlbumFactory');
 module.exports = function(req, res) {
 
     var albumFactory = new AlbumFactory(req.db);
-	var album = albumFactory.get(req.param('uuid')).then(function(album) {
+    albumFactory.get(req.param('uuid')).then(function(album) {
         var isNew = false;
         var error = '';
         var links = {};
 
-		if (album && album.files) {
+        if (album && album.files) {
             files = album.files;
             links.download = config.uploadPath + '/' + album._id + '/archive.zip';
             links.new_album = '/';
-		} else {
+        } else {
             files = [];
             isNew = true;
             log.info('Probably new album ' + req.param('uuid'));
@@ -24,7 +24,7 @@ module.exports = function(req, res) {
         	res.render('index.ejs', {'isnew': false, 'files' : files, 'links': links, 'error':'', 'IMAGE_PATH': albumFactory.PUBLIC_PATH});
         }
 
-	}).catch(function(){
+    }).catch(function(){
         log.warning('Unexisting album ' + req.param('uuid'));
         res.render('index.ejs', {'files' : [], 'error':'Mauvais code !!', 'links': links, 'isnew': false, 'IMAGE_PATH': albumFactory.PUBLIC_PATH});
     });
