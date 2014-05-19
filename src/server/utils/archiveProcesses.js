85883edc7769dadf3dda255214263b57df633702
Matthieu Verrecchia <matthieu.verrecchia@gmail.com>
2014-05-19 14:41:12 +0200

    Added logs and if statement in archive treatment

--- src/server/utils/archiveProcesses.js
+++ src/server/utils/archiveProcesses.js
@@ -8,32 +8,46 @@ module.exports = {
         log.info('albumId pushed to queue : ' + albumId);
         var i = this.fifo.indexOf(albumId);
         if (i === 0) {
+            log.info('albumId already being treated - kill process ' + albumId);
             this.process.kill();
         } else if (i === -1) {
+            log.info('albumId not in queue, pushing it ' + albumId);
             this.fifo.push(albumId);
             this.checkStart();
+        }else{
+            log.info('albumId '+ albumId +' already in queue in position ' + i);
         }
     },
 
     checkStart: function() {
+        log.info('Entering checkstart');
         if (!this.process && this.fifo.length) {
             process.nextTick(function() {
-                var albumId = this.fifo[0];
-                var zipFolder = config.uploadDir + '/' + albumId + '/' + config.originFolderName;
-                var zipFileDestination = config.uploadDir + '/' + albumId + '/archive.zip';
-                var zipCmd = 'cd ' + zipFolder + ' && zip -r ' + zipFileDestination + ' *';
-                log.info('executing ' + zipCmd);
-                this.process = exec(zipCmd, function(err, stdout, stderr) {
-                    this.fifo.shift();
-                    if (!err && !stderr) {
-                        log.info(stdout);
-                    }else{
-                        log.error('Unable to create archive - ' + stderr, err);
-                    }
-                    delete this.process;
-                    this.checkStart();
-                }.bind(this));
+                log.info('Entering nexTick');
+                if (!this.process && this.fifo.length) {
+                    var albumId = this.fifo[0];
+                    var zipFolder = config.uploadDir + '/' + albumId + '/' + config.originFolderName;
+                    var zipFileDestination = config.uploadDir + '/' + albumId + '/archive.zip';
+                    var zipCmd = 'cd ' + zipFolder + ' && zip -r ' + zipFileDestination + ' *';
+                    log.info('executing ' + zipCmd);
+                    this.process = exec(zipCmd, function(err, stdout, stderr) {
+                        this.fifo.shift();
+                        if (!err && !stderr) {
+                            log.info('Archive successfully created for album ' + albumId);
+                            log.info(stdout);
+                        }else{
+                            log.error('Unable to create archive - ' + stderr, err);
+                        }
+                        log.info('Deleting process and relaunch checkStart');
+                        delete this.process;
+                        this.checkStart();
+                    }.bind(this));
+                }else{
+                    log.warning('Exiting checkStart without treatment - after nextTick');
+                }
             }.bind(this));
+        }else{
+            log.warning('Exiting checkStart without treatment - before nextTick');
         }
     }
 };
